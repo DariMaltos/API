fd812453815cac60682b546306408e2c918de3c4
Dariana <dariana.gishel@hotmail.com>
2025-08-28 23:12:54 -0500

    FINAL

--- Parcial1/2_Express/index.js
+++ Parcial1/2_Express/index.js
@@ -3,11 +3,39 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// Middleware para leer JSON
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({ metodo: "GET", mensaje: "Hola desde GET /" }));
-app.post("/", (req, res) => res.json({ metodo: "POST", mensaje: "Recibido en /", body: req.body }));
-app.put("/",  (req, res) => res.json({ metodo: "PUT",  mensaje: "Actualización en /", body: req.body }));
-app.delete("/", (req, res) => res.json({ metodo: "DELETE", mensaje: "Eliminado en /" }));
+// Middleware: solo aceptar peticiones entre 9:00 y 17:00
+app.use((req, res, next) => {
+  const ahora = new Date();
+  const hora = ahora.getHours(); 
 
-app.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
+  if (hora >= 9 && hora < 17) {
+    next(); 
+  } else {
+    res.status(403).send("Petición fuera de horario (permitido 09:00 - 18:00)");
+  }
+});
+
+
+app.get("/", (req, res) =>
+  res.json({ metodo: "GET", mensaje: "Hola desde GET /" })
+);
+
+app.post("/", (req, res) =>
+  res.json({ metodo: "POST", mensaje: "Recibido en /",  body: req.body})
+);
+
+app.put("/", (req, res) =>
+  res.json({ metodo: "PUT", mensaje: "Actualización en /", body: req.body })
+);
+
+app.delete("/", (req, res) =>
+  res.json({ metodo: "DELETE", mensaje: "Eliminado en /" })
+);
+
+// Iniciar servidor
+app.listen(PORT, () =>
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
+);
